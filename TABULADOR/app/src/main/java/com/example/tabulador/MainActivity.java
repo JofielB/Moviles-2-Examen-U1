7f333841912699ca9a70239fcfe69672d2644dd4
DavidBanda <davidbandag96@gmail.com>
2020-03-01 20:43:07 -0700

    update

--- TABULADOR/app/src/main/java/com/example/tabulador/MainActivity.java
+++ TABULADOR/app/src/main/java/com/example/tabulador/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tabulador;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void click_falcon(View v){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        falcon one = new falcon();
+        ft.replace(R.id.frm_layout, one);
+        ft.commit();
+    }
+
+    public void click_ares(View v){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ares_v one = new ares_v();
+        ft.replace(R.id.frm_layout, one);
+        ft.commit();
+    }
+
+    public void click_shuttle(View v){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        shuttle one = new shuttle();
+        ft.replace(R.id.frm_layout, one);
+        ft.commit();
+    }
+
+
 }
